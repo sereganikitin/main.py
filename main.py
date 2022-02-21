70068ce863f1b6086fce7c6a14ff2a9b2d586881
sereganikitin <88857410+sereganikitin@users.noreply.github.com>
2022-02-21 07:29:32 +0300

    Initial Commit

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from PIL import Image
 tree = ET.parse('simvol-67.xml')
 root = tree.getroot()
 
+f = open('parsed.txt', 'w')
+
 for ch in root:
     for child in ch:
         im_tag = str(child.tag)
@@ -16,13 +18,19 @@ for ch in root:
                 im = Image.open(resp)
                 width = im.size[0]
                 height = im.size[1]
-                if width <=1000 or height <= 1000:
+                if width <= 1000 or height <= 1000:
                     print(url, "Размер изображения маловат!: ", width, ' x ', height)
+                    a=(url, "Размер изображения маловат!: ", width, ' x ', height)
+                    f.write(str(a))
+
                 else:
                     print(url, "Размер изображения в норме: ", width, ' x ', height)
+                    b=(url, "Размер изображения в норме: ", width, ' x ', height)
+                    f.write(str(b))
 
             except IOError:
                 print(url, 'Изображение не найдено')
+                c=(url, 'Изображение не найдено')
+                f.write(str(c))
 
-
-
+f.close()
